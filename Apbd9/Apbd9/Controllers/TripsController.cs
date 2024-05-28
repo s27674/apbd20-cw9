f6488f36e1b5b755efeb572b46acf8edf92c3fca
s27674 <s27674@pjwstk.edu.pl>
2024-05-28 16:31:18 +0200

    project

---
+++ Apbd9/Apbd9/Controllers/TripsController.cs
@@ -0,0 +1,24 @@
+﻿using Apbd9.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Apbd9.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TripsController : ControllerBase
+{
+   private readonly Apbd9Context _context;
+   public TripsController(Apbd9Context context)
+   {
+      _context = context;
+   }
+   
+   [HttpGet]
+   public async Task<IActionResult> GetTrips()
+   {
+      var trips = await _context.Trips.Select();
+      return Ok();
+   }
+
+}
